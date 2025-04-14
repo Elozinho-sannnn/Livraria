5640f5775178738e52d9e9e127ed659134ad15b3
Elo <githubchannelzinho@gmail.com>
2025-04-14 12:21:23 -0300

    Add files via upload

---
+++ Livraria/livros.js
@@ -0,0 +1,33 @@
+
+    function criarArquivo(){
+        let livrosTexto = JSON.stringify(livros);
+        cons fs = require('fs');
+        fs.writeFileSync("livros.json", livrosTexto);
+    }
+
+function mostraLivros(){
+    livros.forEach((livro) => {
+        console.log(livro.titulo + "-" +
+                    livro.Autor + "Ano: " +
+                    livro.Ano + "Páginas: " +
+                    livro.Paginas + "Preço: " +
+                    livro.Preco)
+    })
+}
+
+
+function adicionaLivro(titulo, paginas, Autor, Genero, Ano, Preco, Sinopse){
+livros.push({
+    titulo: titulo,
+    paginas: paginas,
+    Autor: Autor,
+    Genero: Genero,
+    Ano: Ano,
+    Preco: Preco,
+    Sinopse: Sinopse
+});
+console.log("livro adicionado com sucesso!");
+}
+adicionaLivro("Trono de vidro", 390, "Sarah j. MAS", "Fantasia", 2020, 25, "Cealena é uma assassina, e a melhor de Adarlan. Encarcerada e fraca, ele está quase perdendo as esperanças quando recebe uma proposta: terá de volta sua liberdad se representar o príncipe de Adarlan em uma competição, lutando contra os mais habilidosos matadores do reino.");
+mostraLivros()
+
